4fa1d083cbc623991ba4701e1252f09f749913cf
plague1981 <42053196+plague1981@users.noreply.github.com>
2020-10-30 08:15:54 -0400

    Update Rhisat2_align_cl.R

--- Rhisat2_align_cl.R
+++ Rhisat2_align_cl.R
@@ -1,7 +1,7 @@
 # ======== Packages required =========
 options(java.parameters = c("-XX:+UseConcMarkSweepGC", "-Xmx16384m"))
 # Rcran
-packages<-c('Xmisc')
+packages<-c('Xmisc','dplyr','R.utils')
 for (package in packages){
   if(package %in% rownames(installed.packages()) == FALSE) {
     install.packages(package)}
@@ -9,12 +9,11 @@ for (package in packages){
 # Bioconductor
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
-bio.packages<-c("Rhisat2")
+bio.packages<-c("Rhisat2",'Rsamtools')
 for (bio.package in bio.packages){
   if(bio.package %in% rownames(installed.packages()) == FALSE) {
     BiocManager::install(bio.package)}
 }
-
 # === setting environment ===
 parser <- Xmisc::ArgumentParser$new()
 parser$add_usage('Rsubread_featureCount_cl.R [options]')
@@ -22,11 +21,11 @@ parser$add_description('An executable R script parsing arguments from Unix-like 
 parser$add_argument('--h',type='logical', action='store_true', help='Print the help page')
 parser$add_argument('--help',type='logical',action='store_true',help='Print the help page')
 parser$add_argument('--dir', type = 'character', default = getwd(), help = '"directory",Enter your working directory')
-
+parser$add_argument('--i', type = 'character', default = 'hg', help = '"index",basename of your index')
+parser$add_argument('--t', type = 'character', default = 'single', help = "c('single','paired')")
 parser$helpme()
 
 # === variables ====
-dir<-'C:/Users/Changyi.Lin/Desktop/Vincent/merged'
 dirPath <- dir
 dirPath <-gsub ('\\\\','/',dirPath)
 if (dir.exists(dirPath)){
@@ -46,8 +45,14 @@ require(dplyr)
 require(Rsamtools)
 
 for (n in 1:length(fastqgz_files)){
-  gunzip(fastqgz_files[n] ) %>% hisat2(sequences =  , index='hg', type="single", outfile=file.path(dir, paste0(fastqgz_files[n],".sam")), force = TRUE)
+  cat(paste0('Processing ',fastqgz_files[n],'...'))
+  gunzip(fastqgz_files[n] ) %>% hisat2(sequences =  , index= i, type= t, outfile=file.path(dir, paste0(fastqgz_files[n],".sam")), force = TRUE)
+  cat('Done!\n')
+  cat(paste0('Recompressing '),sub('fastq.gz','fastq',fastqgz_files[n]),'...')
   gzip(sub('fastq.gz','fastq',fastqgz_files[n]))
-  asBam(file =  , destination =sub(".sam", ".bam",x =  ))  
-  file.remove(paste0(fastqgz_files[1],".sam"))
+  cat('Done!\n')
+  cat(paste0('Converting ', paste0(fastqgz_files[n],".sam"), ' to ',paste0(fastqgz_files[n],".bam...")))
+  asBam(file =paste0(fastqgz_files[n],".sam"), destination =paste0(fastqgz_files[n],".bam"))
+  cat('Done!\n')
+  file.remove(paste0(fastqgz_files[n],".sam"))
 }
